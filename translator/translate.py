816bb2ac23550282438c1fb738a7ac4a1cea2fd1
Bartłomiej Kulenko <coolenko@gmail.com>
2018-09-30 13:11:48 +0200

    changed translate algorithm

--- translator/translate.py
+++ translator/translate.py
@@ -1,41 +1,44 @@
-import re
-
 numdict = {0: '', 1: 'jeden ', 2: 'dwa ', 3: 'trzy ', 4: 'cztery ', 5: 'pięć ', 6: 'sześć ', 7: 'siedem ', 8: 'osiem ', 9: 'dziewięć ', 10: 'dziesięć ',
-           11: 'jedenaście ', 12: 'dwanaście ', 13: 'trzynaście ' , 14: 'czternaście ', 15: 'piętnaście ', 16: 'szesnaście ', 17: 'siedemnaście ',
+           11: 'jedenaście ', 12: 'dwanaście ', 13: 'trzynaście ', 14: 'czternaście ', 15: 'piętnaście ', 16: 'szesnaście ', 17: 'siedemnaście ',
            18: 'osiemnaście ', 19: 'dziewiętnaście ', 20: 'dwadzieścia ', 30: 'trzydzieści ', 40: 'czterdzieści ', 50: 'pięćdziesiąt ',
            60: 'sześćdziesiąt ', 70: 'siedemdziesiąt ', 80: 'osiemdziesiąt ', 90: 'dziewięćdziesiąt ', 100: 'sto ', 200: 'dwieście ',
            300: 'trzysta ', 400: 'czterysta ', 500: 'pięćset ', 600: 'sześćset ', 700: 'siedemset ', 800: 'osiemset ', 900: 'dziewięćset '}
 
-orderDict = {0: ['','',''], 1: ['tysiąc ', 'tysiące ', 'tysięcy '], 2: ['milion ', 'miliony ', 'milionów '], 3: ['miliard ', 'miliardy ', 'miliardów ']}
+orderDict = {0: ['', '', ''], 1: ['tysiąc ', 'tysiące ', 'tysięcy '], 2: ['milion ', 'miliony ', 'milionów '], 3: ['miliard ', 'miliardy ', 'miliardów ']}
 
 
 def translate(number):
-    if len(number) < 12:
-        number = "0" * (12 - len(number)) + number
-    numList = re.findall('.{1,3}', number)
+
+    numList = (format(number, ',d').split(','))
     fin = []
-    iterator = 0
-    if number == '000000000000':
-        return "zero"
-    for sublist in numList:
-        fin.append(numdict[int(sublist[0]) * 100])
-        if sublist[1] == '1':
-            fin.append(numdict[int(sublist[1:])])
+
+    for indx, sublist in enumerate(numList):
+        if len(sublist) >= 2 and sublist[-2] == '1' and sublist[-1] != 0:
+            if len(sublist) == 3:
+                fin.append(numdict[int(sublist[0]) * 100])
+            fin.append(numdict[int(sublist[-2:])])
         else:
-            fin.append(numdict[int(sublist[1]) * 10])
-            fin.append(numdict[int(sublist[2])])
-        if sublist != '000':
-            if sublist == '001':
-                fin.append(orderDict[len(numList) - 1 - iterator][0])
-            elif sublist[1] == '1':
-                fin.append(orderDict[len(numList) - 1 - iterator][2])
+            for i in range(len(sublist)):
+                fin.append(numdict[int(sublist[i]) * (10**(len(sublist) - 1 - i))])
+
+        if len(sublist) >= 2:
+            if sublist[-2] == '1':
+                fin.append(orderDict[len(numList) - 1 - indx][2])
             else:
-                if sublist[2] == '1' or sublist[2] == '0':
-                    fin.append(orderDict[len(numList) - 1 - iterator][2])
-                elif int(sublist[2]) < 5:
-                    fin.append(orderDict[len(numList) - 1 - iterator][1])
+                if sublist == '000':
+                    continue
+                if sublist == '001':
+                    fin.append(orderDict[len(numList) - 1 - indx][0])
+                elif int(sublist[-1]) >= 5 or sublist[-1] == '1' or sublist[-1] == '0':
+                    fin.append(orderDict[len(numList) - 1 - indx][2])
                 else:
-                    fin.append(orderDict[len(numList) - 1 - iterator][2])
-        iterator += 1
-        returnString = ''.join(fin).replace('jeden ', '', (fin.count('jeden ') - 1))
-    return returnString
+                    fin.append(orderDict[len(numList) - 1 - indx][1])
+        else:
+            if sublist[0] == '1':
+                fin.append(orderDict[len(numList) - 1 - indx][0])
+            elif int(sublist[0]) < 5:
+                fin.append(orderDict[len(numList) - 1 - indx][1])
+            else:
+                fin.append(orderDict[len(numList) - 1 - indx][2])
+    fin = [x for x in fin if x != '']
+    return ''.join(fin).replace('jeden miliard', 'miliard').replace('jeden milion', 'milion').replace('jeden tysiąc', 'tysiąc')
